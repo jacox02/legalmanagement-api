83e44eabb27428487d589ca8c56bb2de650d92c5
Jackecx02 <jacobcuevas181@gmail.com>
2022-04-26 19:12:02 -0400

    Update all

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule, {
     cors: true,
